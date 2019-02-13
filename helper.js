34e474224bc169c9709cafcbdc8be920f2e92426
Xuri Li <dlshle@hotmail.com>
2019-02-12 22:37:48 -0600

    finished Schedule class

--- helper.js
+++ helper.js
@@ -1,7 +1,6 @@
 //google maps API https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key=AIzaSyBAz-zWRCP-j42_wjHZyvaIV-0Om1gzswA
-//
 
-var wayOfCommuting, commutingMode, term, sechedule;
+var leastInterval, mostInterval, commutingMode, term, sechedule;
 var allCourses = new Set();
 
 function equals(a,b){
@@ -53,43 +52,119 @@ function Course(title, courseType, sections){
 		this.sections.push(section);
 	}
 	this.indexOfSection = function(section) {
-		for(let i=0;i<sections.length;i++)
-			if(equals(section, sections[i])
+		for(let i=0;i<this.sections.length;i++)
+			if(equals(section, this.sections[i])
 				return i;
 		return -1;
 	}
 }
 
-function Section(course, online, credits, sessons){
+function Section(course, online, credits, sessions){
 	this.course = course;
 	this.online = online;
 	this.credits = credits;
-	this.sessons = [];
-	this.addSesson = function(sesson){
-		this.sessons.push(sesson);
+	this.sessions = [];
+	this.addSesson = function(session){
+		this.sessions.push(sesson);
 	}
-	this.indexOfSesson = function(sesson){
-		for(let i=0;i<sections.length;i++)
-			if(equals(section, sections[i])
+	this.indexOfSesson = function(session){
+		for(let i=0;i<this.sessions.length;i++)
+			if(equals(section, this.sessions[i])
 				return i;
 		return -1;
 	}
 }
 
-
-function Sesson(section, type, sectionNumber, building, date, days, starting, ending){
+let DAY_MAPPING = {"M":0,"T":1, "W":2, "R":3, "F": 4, "SA", 5, "SU", 6};
+function Session(section, type, sectionNumber, building, room, date, days, starting, ending){
 	this.section = section;
 	this.type = type;
-	this.building = building;
+	this.meetings = [];
+	//if there's more than 1 meeting for the week, add'em
+	for(day in days){
+		meeting.push(new Meeting(this, this.building, this.room, DAY_MAPPING[day], this.starting, this.ending));
+	}
 	this.date = date;
-	this.days = days;
-	this.starting = starting;
-	this.ending = ending;
-	this.
 }
 
-function convertMeetingTime(day, start,end){
-	return {Day:day, H:Math.floor(start/6000/60), M:((start/6000)%60)};
+function Meeting(session, building, room, day, start, end){
+	this.session = session;
+	this.building = building;
+	this.room = room;
+	this.day = day;
+	this.start = start;
+	this.end = end;
 }
 
+function isConflict(meeting0, meeting1, commutingMode, leastInterval, mostInterval){
+	if(meeting0.start.time - meeting1.start.time == 0)
+		//exactly the same starting... no way
+		return true;
+	if(meeting0.start.time - meeting1.start.time >= 0)
+		//the premise is to assume meeting0 is before meeting1
+		return isConflict(meeting1, meeting0, commutingMode, leastInterval, mostInterval);	
+	let interval = meeting1.start.time - meeting0.end.time; 
+	if(interval < leastInterval || interval > mostInterval)
+		return true;
+	interval = timeRequiredForTravel(meeting0.session.building, meeting1.session.building, commutingMode, meeting0.start);
+	if(interval < leastInterval || interval > mostInterval)
+		return true;
+	return false;
+}
+
+function Schedule(commutingMode, leastInterval, mostInterval){
+	this.commutingMode = commutingMode;
+	this.leastInterval = leastInterval;
+	this.mostInterval = mostInterval;
+	this.week = [[],[],[],[],[],[],[]];
+
+	this.addSection = function(section){
+		for(let i=0;i<section.sessions.length;i++){
+			if(!this.addSession(section.sessions[i]){
+				for(let j=0;j<i;j++){
+					this.removeSession(section.sessions[j]);
+				}
+				return false;
+			}
+		}
+		return true;
+	}
+
+	this.removeSection = function(section){
+		for(session in section.sessions)
+			this.removeSession(session);
+	}
+
+	this.addSession = function(session){
+		//sorta inefficient algorithm, will edit later...
+		for(meeting in session.meetings)
+			for(m in this.week[m.days])
+				if(isConflict(m, meeting))
+					return false;
+		for(meeting in session.meetings)
+			this.week[meeting.day].push(meeting);
+		return true;
+	}
+
+	this.removeSession = function(session){
+		for(meeting in session.meetings){
+			for(let i=0;i<this.week[meeting.day].length;i++){
+				//linear searching the specific meeting
+				if(equals(this.week[meeting.day][i], meeting))
+					//remove that specific meeting from the week[meeting.day]
+					this.week[meeting.day].splice(i,1);
+			}
+		}
+	}
+
+}
+
+function convertMeetingTime(time){
+	return {time: time, h:Math.floor(start/6000/60), m:((start/6000)%60)};
+}
+
+function getSchedule(allCourses, commutingMode, leastInterval, mostInterval){
+	let schedule = new Schedule(commutingMode, leastInterval, mostInterval);
+	//TODO:make a function that tries every combination and finds the best one
+}
 
